86a6e8bbf6b9d80610fc2a90725dc46019591758
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-02-18 10:38:56 -0300

    add Category and h2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.controllers;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.ok(categories);
+    }
+}
