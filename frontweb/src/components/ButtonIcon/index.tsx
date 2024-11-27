3740e8ad4b101764224b9c63f55263f895b19e12
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-11-26 21:21:51 -0300

    add x-small layout

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function ButtonIcon () {
+    return (
+        <button className="btn btn-primary btn-icon">
+            INICIE AGORA A SUA BUSCA
+        </button>
+    );
+}
+
+export default ButtonIcon;
---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css';
+import ButtonIcon from 'components/ButtonIcon';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-card">
+        <div className="home-content-container">
+          <h1>Conheça o melhor catálogo de produtos</h1>
+          <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+          <ButtonIcon />
+        </div>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
